3048b739551b362abf9c3c1bad9863861188b12f
Johannes <Johannes.Nolte@stud.fh-rosenheim.de>
2019-09-29 23:10:14 +0200

    added basic mpv controll ans remote control functions

---
+++ remotec/commandprocess.py
@@ -0,0 +1,264 @@
+import multiprocessing
+import threading
+import queue
+import socket
+import telnetlib
+import time
+import re
+
+import logging
+import sys
+import os
+
+from remotec.commandobj import CommandObj, dict_commandobj, atlona, dnc
+
+class CommandProcess:
+
+    @staticmethod
+    def command_process(command_pipe, logger):
+
+        if not logger:
+            logger = logging.getLogger()
+            logger.setLevel(logging.DEBUG)
+
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setLevel(logging.DEBUG)
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+
+        logger.info("Started command_process with pid {}".format(os.getpid()))
+
+        #load dictionary containing commands from yaml file
+        dict_atlona = dict_commandobj("remotec/cmdatlona.yaml")
+
+        #create quee for communication with thread
+        q_send_atlona = queue.Queue()
+        q_recv_atlona = queue.Queue()
+        q_stat_atlona = queue.Queue()
+
+        #create and start thread
+        thread_atlona = threading.Thread(
+            target=CommandProcess.listening_atlona, 
+            name='thread_atlona',
+            args=(q_send_atlona, 
+                q_recv_atlona, 
+                q_stat_atlona, 
+                dict_atlona,
+                logger))
+        thread_atlona.start()
+
+        #load dictionary containing commands from yaml file
+        dict_denon = dict_commandobj("remotec/cmddenon.yaml")
+
+        #create quee for communication with thread
+        q_send_denon = queue.Queue()
+        q_recv_denon = queue.Queue()
+        q_stat_denon = queue.Queue()
+
+        #create and start thread
+        thread_denon = threading.Thread(
+            target=CommandProcess.listening_denon, 
+            name='thread_denon',
+            args=(q_send_denon, 
+                q_recv_denon, 
+                q_stat_denon, 
+                dict_denon,
+                ),
+            kwargs=({'loga': logger,}))
+        thread_denon.start()
+
+        while True:
+            if command_pipe.poll():
+                command_pipe.recv()
+                #logger.info(data)
+                q_send_atlona.put(dict_atlona.get('Set Output').send_command(2, 1))
+                q_send_denon.put(dict_denon.get('Track Jump').send_command(1))
+
+
+    @staticmethod
+    def listening_atlona(q_send, q_recv, q_stat, dict_c=None, loga=None):
+        """Thread function for listening and writing via telnet protocoll
+
+        Sends commands in q_send with an delay of 500ms delay between each
+        command and continuesly listens for incomming messages. Prints all
+        send and recived messages to stdout
+            
+        Args:
+            q_send (queue.Queue): Queue for commands to be send.
+            q_recv (queue.Queue): Queue for returning answers of commands q_send
+            q_stat (queue.Queue): Queue for returning stat messages recived
+            dict_c (dict, optional): dictionary of commands for device
+
+        """
+
+        loga.info("started thread 'listening_atlona'")
+        IP_ADDR = '192.168.178.202'
+
+        last_send_data = b''        
+        last_send_time = time.time()
+        echo_recived = False
+        
+        #counter for testing, will be 0 or one when no stat change or error 
+        tmp_n = 1
+
+        #connect to switch via tellnet ...
+        with telnetlib.Telnet(IP_ADDR, port=23, timeout=5) as tn:
+            tn.set_debuglevel(0)
+            #and wait until welcome message is recived
+            tn.read_until(b'Welcome to TELNET.\r\n')
+            #while loop of thread
+            while True:
+                time_tmp = time.time()
+                #only send new command from queue conditions are met:
+                #  -500ms between commands
+                #  -not waiting for echo of a prev command
+                #  -queue not empty
+                if time_tmp-last_send_time > .5 \
+                        and not echo_recived \
+                        and not q_send.empty():
+                    val = q_send.get()
+                    loga.info("Send: {0:<78}R{0}".format(str(val.encode())))
+                    last_send_data = val.encode()
+                    tn.write(last_send_data)
+                    last_send_time = time_tmp
+
+                #allways try to recive messages with given end sequence
+                byt_recv = tn.read_until(b'\n', timeout=.1)
+
+                #just for debuging: print plain recived data
+                if(byt_recv):
+                    if echo_recived: 
+                        tmp = 'T'
+                        tmp_n = tmp_n+1
+                    else: 
+                        tmp = 'F'
+                        tmp_n=tmp_n-1
+                    loga.info("{:<85}{}{:<30}{}".format('-', tmp, str(byt_recv), tmp_n))
+
+                #check if recived massage is valid         
+                if(byt_recv and byt_recv.endswith(b'\r\n')):
+                    #if its equal the send message its the echo sen by client
+                    #therefore continue and recive the wanted answer                    
+                    if not echo_recived and last_send_data in byt_recv:
+                        echo_recived = True
+                        continue
+
+                    #decode mesage and find the corresponding entyr in the
+                    #dictionary (when provided)
+                    str_recv = byt_recv.decode().rstrip()
+                    if(dict_c):
+                        value = dict_c.get_full_answer(str_recv)
+                    else:
+                        value = (None, str_recv)
+
+                    #if an echo was recived a command was send bevore,
+                    #else a status message was sen from the client
+                    if echo_recived:
+                        #if command failed
+                        if re.search(atlona.error_seq, str_recv):
+                            loga.info("Echo: {}".format(str_recv))
+                            q_stat.put(str_recv)
+                        else:
+                            loga.info("Echo: {0:<30} --> {1:<43}E{0}".format(str(byt_recv), str(value)))
+                            q_recv.put(str_recv)
+                            q_stat.put(str_recv)
+                        echo_recived=False
+                    else:
+                        loga.info("Stat: {0:<30} --> {1:<43}S{0}".format(str(byt_recv), str(value)))
+                        q_stat.put(str_recv)
+
+    @staticmethod
+    def listening_denon(q_send, q_recv, q_stat, dict_c=None, loga=None):
+        """Thread function for listening and writing via telnet protocoll
+
+        Sends commands in q_send with an delay of 500ms delay between each
+        command and continuesly listens for incomming messages. Prints all
+        send and recived messages to stdout
+            
+        Args:
+            q_send (queue.Queue): Queue for commands to be send.
+            q_recv (queue.Queue): Queue for returning answers of commands q_send
+            q_stat (queue.Queue): Queue for returning stat messages recived
+            dict_c (dict, optional): dictionary of commands for device
+
+        """
+
+        loga.info("started thread 'listening_denon'")
+
+        TCP_IP = '192.168.178.201'
+        TCP_PORT = 9030
+        BUFFER_SIZE = 1024
+        
+        #creteat socket and connect ...
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((TCP_IP, TCP_PORT))
+
+            #timeout for socket.recv, also enssures 20ms between each send
+            s.settimeout(.02)
+
+            #while loop of thread
+            while True:
+                #only send new command from queue conditions are met:
+                #  -queue not empty
+                if not q_send.empty():
+                    val = q_send.get()
+                    loga.info("Send: {}".format(val))
+                    s.send(val.encode())
+
+                #listent to socket for incomming messages until timeout
+                try:
+                    str_recv = s.recv(BUFFER_SIZE).decode()
+                except socket.timeout:
+                    str_recv = None
+
+                #when a string was recived extract message and type
+                if str_recv:
+                    #split strings when multiple commands are contained 
+                    m = re.findall(r'((?:ack\+\@|@0\?{0,1}|ack\+)(?:\w|\d)*)', 
+                        str_recv)
+                    if m:
+                        for data in m:
+                            #data is answer of device
+                            if data.startswith('ack+'):
+                                if(dict_c):
+                                    loga.info("Recv: {:<20} --> {}".format(
+                                        data, dict_c.get_full_answer(data)))
+                                else:
+                                    loga.info("Recv: {:<20}".format(data))
+                                q_recv.put(data)
+                            #data is a status information
+                            elif data.startswith('@0'):
+                                #TODO does not work, bd expects ack
+                                s.send('ACK'.encode())
+                                if(dict_c):
+                                    loga.info("Stat: {:<20} --> {}".format(
+                                        data, dict_c.get_full_answer(data)))
+                                else:
+                                    loga.info("Stat: {:<20}".format(data))
+                                q_stat.put(data)
+
+#import error will be rised
+if __name__ == "__main__":
+
+    def wait_for_enter(pipeA):
+        input("Press Enter to Start BluRay")
+        pipeA.send("start")
+
+    pipeA, pipeB = multiprocessing.Pipe()
+
+    process = multiprocessing.Process(
+        target=CommandProcess.command_process, 
+        name="commandprocess",
+        args=(pipeB, ))
+    process.daemon = True
+    process.start()
+    
+    #x = input("Press Enter to continue...")
+    
+    #pipeA.send("start")  
+
+    t = threading.Thread(target=wait_for_enter, args=(pipeA,))
+    t.start()
+
+    process.join()
--- viewcontrol.py
+++ viewcontrol.py
@@ -8,6 +8,18 @@ import os
 import sys
 import yaml
 
+import functools
+
+#from pynput.keyboard import Key, Listener
+
+try: 
+    import mpv
+except OSError:
+    print("libmpv not found please install. See debug log for traceback.")
+    raise
+
+from mediaelement import MediaElement, VideoElement, StillElement, Command
+from remotec.commandprocess import CommandProcess
 
 class ViewControl(object):
 
@@ -67,40 +79,125 @@ class ViewControl(object):
                 "Config File {} not found, can't start programm!" \
                 .format(os.path.abspath(config_path)))
 
-        self.processA = multiprocessing.Process(target=self.process_A, name="processA", args=(self.config_queue_logger,) )
-        self.processA.daemon = True
+        #self.pipe_pc, self.pipe_cc = multiprocessing.Pipe()
+        self.pipe_mpv_time_A, self.pipe_mpv_time_B = multiprocessing.Pipe()
+        self.pipe_mpv_stat_A, self.pipe_mpv_stat_B = multiprocessing.Pipe()
+        self.pipe_mpv_apnd_A, self.pipe_mpv_apnd_B = multiprocessing.Pipe()
 
-        self.processB = multiprocessing.Process(target=self.process_B, name="processB", args=(self.config_queue_logger,) )
-        self.processB.daemon = True
+        pipeA, pipeB = multiprocessing.Pipe()
+
+        self.process_cmd = multiprocessing.Process(
+            target=CommandProcess.command_process, 
+            name="process_cmd", 
+            args=(pipeB, self.logger))
+        self.process_cmd.daemon = True
+
+        self.process_mpv = multiprocessing.Process(
+            target=self.def_process_mpv, 
+            name="process_mpv", 
+            args=(self.config_queue_logger, 
+                self.pipe_mpv_time_B, 
+                self.pipe_mpv_stat_B, 
+                self.pipe_mpv_apnd_A,))
+        self.process_mpv.daemon = True
 
         self.processeses = []
-        self.processeses.append(self.processA)
-        self.processeses.append(self.processB)
+        self.processeses.append(self.process_cmd)
+        self.processeses.append(self.process_mpv)
 
-    def process_A(self, logger_config):
-        logging.config.dictConfig(logger_config)
-        logger = logging.getLogger("process_A")
-        logger.info("Started process_A")
-        try:
-            while True:
-                time.sleep(1)
-                logger.debug("Hello")
-                time.sleep(5)
-                raise RuntimeError("Test unhandled")
-        except Exception as e:
-            try:
-                raise
-            finally:
-                logger.error("Uncaught exception", exc_info=(e))    
+        self.logger.info("Initialized __main__ with pid {}".format(os.getpid()))
 
-    def process_B(self, logger_config):
+        #Only For Pre-Alpha Version
+        t = threading.Thread(
+            target=ViewControl.wait_for_enter, 
+            name='pre-alpha', 
+            args=(pipeA,))
+        t.start()
+
+    #Only For Pre-Alpha Version
+    @staticmethod
+    def wait_for_enter(pipeA):
+        input("Press Enter to Start BluRay")
+        pipeA.send("start")
+
+    # def def_process_cmd(self, logger_config):
+    #     """example process definition"""
+    #     logging.config.dictConfig(logger_config)
+    #     logger = logging.getLogger("process_A")
+    #     logger.info("Started process_A with pid {}".format(os.getpid()))
+    #     try:
+    #         while True:
+    #             time.sleep(1)
+    #     except Exception as e:
+    #         try:
+    #             raise
+    #         finally:
+    #             logger.error("Uncaught exception", exc_info=(e))    
+
+    def mpv_log(self, loglevel, component, message, log):
+        if loglevel == "fatal":
+            level = 50
+        elif loglevel == "error":
+            level = 40
+        elif loglevel == "warn":
+            level = 30
+        elif loglevel =="info":
+            level = 20
+        elif logging in ["v", "debug", "trace"]:
+            level = 10
+        else:
+            level = 0
+        log.log(level, "MPV:" + message)
+
+
+    def mpv_observer_time(self, prop, value, log, pipe):
+            pipe.send((prop, value))
+
+
+    def mpv_observer_stat(self, prop, value, log, pipe):
+            pipe.send((prop, value))
+
+
+    def def_process_mpv(self, logger_config, 
+        pipe_mpv_time, pipe_mpv_statl, pipe_mpv_apnd):
         logging.config.dictConfig(logger_config)
-        logger = logging.getLogger("process_B")
-        logger.info("Started process_B")
+        logger = logging.getLogger("process_mpv")
+        logger.info("Started process_mpv with pid {}".format(os.getpid()))
+        # mpv expects logger func without a logger, therfore create new funtion
+        # with log already filled in  
         try:
+            mpv_log = functools.partial(self.mpv_log, log=logger)
+            player = mpv.MPV(log_handler=mpv_log, ytdl=False)
+            player.fullscreen = True
+            player['image-display-duration'] = 5  # Pipe in while loop to update duration
+            player['keep-open'] = True
+            player['osc'] = False
+            player.loop_playlist = 'inf'
+
+            #handler_mpv_observer_time = functools.partial(self.mpv_observer_time, log=logger, pipe=pipe_mpv_time)
+            #player.observe_property('time-remaining', handler_mpv_observer_time)
+            #player.observe_property('time-pos', handler_mpv_observer_time)
+            #handler_mpv_observer_stat = functools.partial(self.mpv_observer_stat, log=logger, pipe=pipe_mpv_statl)
+            #player.observe_property('filename', handler_mpv_observer_stat)
+            #player.observe_property('playlist', handler_mpv_observer_stat)
+            #player.observe_property('playlist-pos', handler_mpv_observer_stat)
+            #player.observe_property('playlist-pos-1', handler_mpv_observer_stat)
+            #player.observe_property('playlist-count', handler_mpv_observer_stat)
+            
+            player.playlist_append('viewcontrol.png')
+            #logger.info("disptime: {}".format(player['image-display-duration']))
+            player.playlist_append('pic1_im_moon.jpg')
+            player.playlist_append('pic2_im_pda.jpg')
+            player.playlist_append('pic3_im_shark.jpg')
+
             while True:
-                time.sleep(1)
-                logger.debug("World")
+                if pipe_mpv_apnd.poll():
+                    data = pipe_mpv_apnd.recv()                   
+                    player.playlist_append(data.file_path)
+                    #if(isinstance(data, StillElement)):
+                        #player['image-display-duration'] = data.display_time
+                    logger.info("Appending File {} at pos {} in playlist.".format(str(data), len(player.playlist)))
+                
         except Exception as e:
             try:
                 raise
@@ -111,14 +208,84 @@ class ViewControl(object):
         self.logger.info("StartedingProcessses")
         for process in self.processeses:
             process.start()
-        while True:
-            for process in self.processeses:
-                if not process.is_alive():
-                    exc_msg = "Uncaught exception in subprocess: '{}'".format(process.name)
-                    self.logger.error(exc_msg)
-                    for p in multiprocessing.active_children():
-                        p.terminate()
-                    raise Exception(exc_msg)
+
+        # poped = False
+
+        # pic1 = StillElement('pic1', 'pic1_im_moon.jpg', display_time=8)
+        # pic2 = StillElement('pic2', 'pic2_im_pda.jpg', Command('Hello', 'World', 3), display_time=13)
+        # pic3 = StillElement('pic3', 'pic3_im_shark.jpg', display_time=7)
+        # pic4 = StillElement('pic4', 'pic4_IMG_3311.JPG', display_time=3)
+
+        # vid1 = VideoElement('vid1', 'vid1_forever.mkv')
+        # vid2 = VideoElement('vid2', 'vid2.mp4', Command('fuu', 'bar', 3))
+        # vid3 = VideoElement('vid3', 'vid3.mp4', [Command('fuu2', 'bar2', 2), Command('fuu4', 'bar4', 4)])
+        # vid4 = VideoElement('vid4', 'vid4_fire.mp4')
+        
+        # listloop = [vid2, pic1, pic2, pic3, pic4, ]
+
+        # element_current = pic3
+        # element_next = None
+        # timerlist = None
+
+        # while True:            
+        #     if self.pipe_mpv_stat_A.poll():
+        #         data = self.pipe_mpv_stat_A.recv()
+        #         if data[0] == 'playlist-pos-1':
+        #             self.logger.info("recived playlist pos {}".format(data))
+        #             poped = False
+        #         elif data[0] == 'filename':
+        #             self.logger.info("recived filename {}".format(data))
+        #             if data[1]=='viewcontrol.png':
+        #                 print("fuu")
+        #                 continue
+        #             elif data[1]==element_next.file_path:
+        #                 print("bar")             
+        #                 element_current = element_next
+        #                 timerlist = []
+        #                 if element_current.list_commands:
+        #                     for me in element_current.list_commands:
+        #                         if me:
+        #                             timerlist.append(threading.Timer(
+        #                                 interval=me.delay, 
+        #                                 function=self.runcommand, 
+        #                                 args=(me.name, self.logger)))
+        #                     for t in timerlist:
+        #                         t.start()
+        #     elif not poped and isinstance(element_current, StillElement):
+        #         if len(listloop) > 0:
+        #             media_element = listloop.pop(0)
+        #             self.pipe_mpv_apnd_B.send(media_element)
+        #             element_next = media_element
+        #             poped = True
+        #     elif self.pipe_mpv_time_A.poll():
+        #         data = self.pipe_mpv_time_A.recv()
+        #         #self.logger.debug("recived time {}".format(data))
+        #         if not poped and len(listloop) > 0:
+        #             if data[1] <= 1:
+        #                 media_element = listloop.pop(0)
+        #                 self.pipe_mpv_apnd_B.send(media_element)
+        #                 element_next = media_element
+        #                 poped = True
+        #     #elif not element_next:
+        #     #    if not poped and len(listloop) > 0:
+        #     #        media_element = listloop.pop(0)
+        #     #        self.pipe_mpv_apnd_B.send(media_element)
+        #     #        element_next = media_element
+        #     #        poped = True
+            
+        for process in self.processeses:
+            if not process.is_alive():
+                exc_msg = "Uncaught exception in subprocess: '{}'".format(process.name)
+                self.logger.error(exc_msg)
+                for p in multiprocessing.active_children():
+                    p.terminate()
+                raise Exception(exc_msg)
+            #time.sleep(.1)
+
+        
+
+    def runcommand(self, cmdobj, cmdqueue):
+        cmdqueue.info("now command '{}' will be send to device".format(cmdobj))
 
     def handle_exception(self, exc_type, exc_value, exc_traceback):
         """
@@ -152,6 +319,7 @@ class ViewControl(object):
             logger.handle(record)
 
 
+  
 if __name__ == "__main__":
     test = ViewControl()
     test.run()
